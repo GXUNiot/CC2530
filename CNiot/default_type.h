7ac4f167bcf011a4954c574b3aacbae055fd6c65
蒙良桥 <15977877715@qq.com>
2019-04-05 02:52:37 +0800

    数据类型定义

---
+++ CNiot/default_type.h
@@ -0,0 +1,35 @@
+/*****************************************
+ * 文件名：   default_type.h
+ * 
+ * 说明：     定义基本类型
+ * 
+ * 
+ * 作者：     汜方(15977877715@qq.com)
+ * 修改时间：   2019-02-27
+ *****************************************/
+
+#ifndef _DEFAULT_TYPE_H
+#define _DEFAULT_TYPE_H
+
+/*****************  库  *****************/
+
+
+
+
+/***************** 定义 *****************/
+
+typedef signed char     int8_t;
+typedef unsigned char   uint8_t;
+typedef signed int      int16_t;
+typedef unsigned int    uint16_t;
+typedef signed long     int32_t;
+typedef unsigned long   uint32_t; 
+
+
+
+
+
+
+
+
+#endif //_DEFAULT_TYPE_H

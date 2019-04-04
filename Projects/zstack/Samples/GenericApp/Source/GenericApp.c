677b5ffcd4447546d9a67e0b917df44a1178578d
蒙良桥 <15977877715@qq.com>
2019-04-05 02:54:33 +0800

    Z-Stack 协议栈

---
+++ Projects/zstack/Samples/GenericApp/Source/GenericApp.c
@@ -0,0 +1,549 @@
+/**************************************************************************************************
+  Filename:       GenericApp.c
+  Revised:        $Date: 2009-03-18 15:56:27 -0700 (Wed, 18 Mar 2009) $
+  Revision:       $Revision: 19453 $
+
+  Description:    Generic Application (no Profile).
+
+
+  Copyright 2004-2009 Texas Instruments Incorporated. All rights reserved.
+
+  IMPORTANT: Your use of this Software is limited to those specific rights
+  granted under the terms of a software license agreement between the user
+  who downloaded the software, his/her employer (which must be your employer)
+  and Texas Instruments Incorporated (the "License").  You may not use this
+  Software unless you agree to abide by the terms of the License. The License
+  limits your use, and you acknowledge, that the Software may not be modified,
+  copied or distributed unless embedded on a Texas Instruments microcontroller
+  or used solely and exclusively in conjunction with a Texas Instruments radio
+  frequency transceiver, which is integrated into your product.  Other than for
+  the foregoing purpose, you may not use, reproduce, copy, prepare derivative
+  works of, modify, distribute, perform, display or sell this Software and/or
+  its documentation for any purpose.
+
+  YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
+  PROVIDED “AS IS?WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, 
+  INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE, 
+  NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
+  TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
+  NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
+  LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+  INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
+  OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
+  OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+  (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+  Should you have any questions regarding your right to use this Software,
+  contact Texas Instruments Incorporated at www.TI.com. 
+**************************************************************************************************/
+
+/*********************************************************************
+  This application isn't intended to do anything useful, it is
+  intended to be a simple example of an application's structure.
+
+  This application sends "Hello World" to another "Generic"
+  application every 15 seconds.  The application will also
+  receive "Hello World" packets.
+
+  The "Hello World" messages are sent/received as MSG type message.
+
+  This applications doesn't have a profile, so it handles everything
+  directly - itself.
+
+  Key control:
+    SW1:
+    SW2:  initiates end device binding
+    SW3:
+    SW4:  initiates a match description request
+*********************************************************************/
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "OSAL.h"
+#include "AF.h"
+#include "ZDApp.h"
+#include "ZDObject.h"
+#include "ZDProfile.h"
+
+#include "GenericApp.h"
+#include "DebugTrace.h"
+
+#if !defined( WIN32 )
+  #include "OnBoard.h"
+#endif
+
+/* HAL */
+#include "hal_lcd.h"
+#include "hal_led.h"
+#include "hal_key.h"
+#include "hal_uart.h"
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/*********************************************************************
+ * MACROS
+ */
+
+/*********************************************************************
+ * CONSTANTS
+ */
+
+/*********************************************************************
+ * TYPEDEFS
+ */
+
+/*********************************************************************
+ * GLOBAL VARIABLES
+ */
+
+// This list should be filled with Application specific Cluster IDs.
+const cId_t GenericApp_ClusterList[GENERICAPP_MAX_CLUSTERS] =
+{
+  GENERICAPP_PERIODIC_CLUSTERID,
+  GENERICAPP_FLASH_CLUSTERID,
+  GENERICAPP_P2P_CLUSTERID
+};
+
+const SimpleDescriptionFormat_t GenericApp_SimpleDesc =
+{
+  GENERICAPP_ENDPOINT,              //  int Endpoint;
+  GENERICAPP_PROFID,                //  uint16 AppProfId[2];
+  GENERICAPP_DEVICEID,              //  uint16 AppDeviceId[2];
+  GENERICAPP_DEVICE_VERSION,        //  int   AppDevVer:4;
+  GENERICAPP_FLAGS,                 //  int   AppFlags:4;
+  GENERICAPP_MAX_CLUSTERS,          //  byte  AppNumInClusters;
+  (cId_t *)GenericApp_ClusterList,  //  byte *pAppInClusterList;
+  GENERICAPP_MAX_CLUSTERS,          //  byte  AppNumInClusters;
+  (cId_t *)GenericApp_ClusterList   //  byte *pAppInClusterList;
+};
+
+// This is the Endpoint/Interface description.  It is defined here, but
+// filled-in in GenericApp_Init().  Another way to go would be to fill
+// in the structure here and make it a "const" (in code space).  The
+// way it's defined in this sample app it is define in RAM.
+endPointDesc_t GenericApp_epDesc;
+
+/*********************************************************************
+ * EXTERNAL VARIABLES
+ */
+
+/*********************************************************************
+ * EXTERNAL FUNCTIONS
+ */
+
+/*********************************************************************
+ * LOCAL VARIABLES
+ */
+byte GenericApp_TaskID;   // Task ID for internal task/event processing
+                          // This variable will be received when
+                          // GenericApp_Init() is called.
+devStates_t GenericApp_NwkState;
+
+
+byte GenericApp_TransID;  // This is the unique message ID (counter)
+
+afAddrType_t GenericApp_Periodic_DstAddr;
+afAddrType_t GenericApp_Flash_DstAddr;
+afAddrType_t GenericApp_P2P_DstAddr;
+
+
+
+
+/*********************************************************************
+ * LOCAL FUNCTIONS
+ */
+void GenericApp_ProcessZDOMsgs( zdoIncomingMsg_t *inMsg );
+void GenericApp_HandleKeys( byte shift, byte keys );
+void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pckt );
+void GenericApp_SendTheMessage( void );
+
+
+
+/*********************************************************************
+ * NETWORK LAYER CALLBACKS
+ */
+
+/*********************************************************************
+ * PUBLIC FUNCTIONS
+ */
+
+/*********************************************************************
+ * @fn      GenericApp_Init
+ *
+ * @brief   Initialization function for the Generic App Task.
+ *          This is called during initialization and should contain
+ *          any application specific initialization (ie. hardware
+ *          initialization/setup, table initialization, power up
+ *          notificaiton ... ).
+ *
+ * @param   task_id - the ID assigned by OSAL.  This ID should be
+ *                    used to send messages and set timers.
+ *
+ * @return  none
+ */
+void GenericApp_Init( byte task_id )
+{
+  GenericApp_TaskID = task_id;
+  GenericApp_NwkState = DEV_INIT;
+  GenericApp_TransID = 0;
+
+  // Device hardware initialization can be added here or in main() (Zmain.c).
+  // If the hardware is application specific - add it here.
+  // If the hardware is other parts of the device add it in main().
+
+
+
+  GenericApp_Periodic_DstAddr.addrMode = (afAddrMode_t)AddrBroadcast;
+  GenericApp_Periodic_DstAddr.endPoint = GENERICAPP_ENDPOINT;
+  GenericApp_Periodic_DstAddr.addr.shortAddr = 0xFFFF;
+
+  GenericApp_Flash_DstAddr.addrMode = (afAddrMode_t)afAddrGroup;
+  GenericApp_Flash_DstAddr.endPoint = GENERICAPP_ENDPOINT;
+  GenericApp_Flash_DstAddr.addr.shortAddr = GENERIC_FLASH_GROUP;
+
+  GenericApp_P2P_DstAddr.addrMode = (afAddrMode_t)Addr16Bit;
+  GenericApp_P2P_DstAddr.endPoint = GENERICAPP_ENDPOINT;
+  GenericApp_P2P_DstAddr.addr.shortAddr = 0x0000;
+  
+
+
+
+
+  // Fill out the endpoint description.
+  GenericApp_epDesc.endPoint = GENERICAPP_ENDPOINT;
+  GenericApp_epDesc.task_id = &GenericApp_TaskID;
+  GenericApp_epDesc.simpleDesc
+            = (SimpleDescriptionFormat_t *)&GenericApp_SimpleDesc;
+  GenericApp_epDesc.latencyReq = noLatencyReqs;
+
+  // Register the endpoint description with the AF
+  afRegister( &GenericApp_epDesc );
+
+  // Register for all key events - This app will handle all key events
+  RegisterForKeys( GenericApp_TaskID );
+
+  // Update the display
+#if defined ( LCD_SUPPORTED )
+    HalLcdWriteString( "GenericApp", HAL_LCD_LINE_1 );
+#endif
+    
+  ZDO_RegisterForZDOMsg( GenericApp_TaskID, End_Device_Bind_rsp );
+  ZDO_RegisterForZDOMsg( GenericApp_TaskID, Match_Desc_rsp );
+
+
+
+
+#ifdef CNIOT  
+
+  setup();
+
+  osal_start_timerEx( GenericApp_TaskID, CNIOT_LOOP_TASK_EVT, CNIOT_LOOP_TASK_TIMEOUT*10 );
+
+#endif
+
+}
+
+/*********************************************************************
+ * @fn      GenericApp_ProcessEvent
+ *
+ * @brief   Generic Application Task event processor.  This function
+ *          is called to process all events for the task.  Events
+ *          include timers, messages and any other user defined events.
+ *
+ * @param   task_id  - The OSAL assigned task ID.
+ * @param   events - events to process.  This is a bit map and can
+ *                   contain more than one event.
+ *
+ * @return  none
+ */
+UINT16 GenericApp_ProcessEvent( byte task_id, UINT16 events )
+{
+  afIncomingMSGPacket_t *MSGpkt;
+  afDataConfirm_t *afDataConfirm;
+
+  // Data Confirmation message fields
+  byte sentEP;
+  ZStatus_t sentStatus;
+  byte sentTransID;       // This should match the value sent
+  (void)task_id;  // Intentionally unreferenced parameter
+
+  if ( events & SYS_EVENT_MSG )
+  {
+    MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( GenericApp_TaskID );
+    while ( MSGpkt )
+    {
+      switch ( MSGpkt->hdr.event )
+      {
+        case ZDO_CB_MSG:
+          GenericApp_ProcessZDOMsgs( (zdoIncomingMsg_t *)MSGpkt );
+          break;
+          
+        case KEY_CHANGE:
+          GenericApp_HandleKeys( ((keyChange_t *)MSGpkt)->state, ((keyChange_t *)MSGpkt)->keys );
+          break;
+
+        case AF_DATA_CONFIRM_CMD:
+          // This message is received as a confirmation of a data packet sent.
+          // The status is of ZStatus_t type [defined in ZComDef.h]
+          // The message fields are defined in AF.h
+          afDataConfirm = (afDataConfirm_t *)MSGpkt;
+          sentEP = afDataConfirm->endpoint;
+          sentStatus = afDataConfirm->hdr.status;
+          sentTransID = afDataConfirm->transID;
+          (void)sentEP;
+          (void)sentTransID;
+
+          // Action taken when confirmation is received.
+          if ( sentStatus != ZSuccess )
+          {
+            // The data wasn't delivered -- Do something
+          }
+          break;
+
+        case AF_INCOMING_MSG_CMD:
+          GenericApp_MessageMSGCB( MSGpkt );
+          break;
+
+        case ZDO_STATE_CHANGE:
+          GenericApp_NwkState = (devStates_t)(MSGpkt->hdr.status);
+          #ifdef CNIOT
+
+          NETState_Process(GenericApp_NwkState);
+
+          #else
+
+          if ( (GenericApp_NwkState == DEV_ZB_COORD)
+              || (GenericApp_NwkState == DEV_ROUTER)
+              || (GenericApp_NwkState == DEV_END_DEVICE) )
+          {
+            // Start sending "the" message in a regular interval.
+  
+          }
+
+          #endif
+          break;
+
+        default:
+          break;
+      }
+
+      // Release the memory
+      osal_msg_deallocate( (uint8 *)MSGpkt );
+
+      // Next
+      MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( GenericApp_TaskID );
+    }
+
+    // return unprocessed events
+    return (events ^ SYS_EVENT_MSG);
+  }
+
+
+
+#ifdef CNIOT  
+
+  if ( events & CNIOT_LOOP_TASK_EVT )
+  {
+    // Send "the" message
+    loop();
+
+    // Setup to send message again
+    osal_start_timerEx( GenericApp_TaskID, CNIOT_LOOP_TASK_EVT, CNIOT_LOOP_TASK_TIMEOUT );
+
+    // return unprocessed events
+    return (events ^ CNIOT_LOOP_TASK_EVT);
+  }
+
+#endif
+
+  // Discard unknown events
+  return 0;
+}
+
+
+/*********************************************************************
+ * Event Generation Functions
+ */
+
+/*********************************************************************
+ * @fn      GenericApp_ProcessZDOMsgs()
+ *
+ * @brief   Process response messages
+ *
+ * @param   none
+ *
+ * @return  none
+ */
+void GenericApp_ProcessZDOMsgs( zdoIncomingMsg_t *inMsg )
+{
+  switch ( inMsg->clusterID )
+  {
+    case End_Device_Bind_rsp:
+
+
+    case Match_Desc_rsp:
+      {
+        ZDO_ActiveEndpointRsp_t *pRsp = ZDO_ParseEPListRsp( inMsg );
+        if ( pRsp )
+        {
+          if ( pRsp->status == ZSuccess && pRsp->cnt )
+          {
+            GenericApp_P2P_DstAddr.addrMode = (afAddrMode_t)Addr16Bit;
+            GenericApp_P2P_DstAddr.addr.shortAddr = pRsp->nwkAddr;
+            // Take the first endpoint, Can be changed to search through endpoints
+            GenericApp_P2P_DstAddr.endPoint = pRsp->epList[0];
+
+          }
+          osal_mem_free( pRsp );
+        }
+      }
+      break;
+  }
+}
+
+
+void GenericApp_HandleKeys( byte shift, byte keys )
+{
+  //zAddrType_t dstAddr;
+  
+  
+}
+
+/*********************************************************************
+ * LOCAL FUNCTIONS
+ */
+
+/*********************************************************************
+ * @fn      GenericApp_MessageMSGCB
+ *
+ * @brief   Data message processor callback.  This function processes
+ *          any incoming data - probably from other devices.  So, based
+ *          on cluster ID, perform the intended action.
+ *
+ * @param   none
+ *
+ * @return  none
+ */
+void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pkt )
+{
+#ifdef CNIOT
+  char buf[AIR_BUF_MAXLEN+2];
+  sscanf((char*)pkt->cmd.Data,"%[^$]",buf);
+  MessageProcess(pkt->srcAddr.addr.shortAddr, (uint8_t*)buf);
+  //MessageProcess(pkt->srcAddr.addr.shortAddr, (char*)pkt->cmd.Data);
+#else
+
+  switch ( pkt->clusterId )
+  {
+    case GENERICAPP_PERIODIC_CLUSTERID:
+
+      break;
+    case GENERICAPP_FLASH_CLUSTERID:
+
+      break;
+    case GENERICAPP_P2P_CLUSTERID:
+
+      break;
+  }
+#endif
+}
+
+
+
+/*********************************************************************
+ * 
+ * 
+ * 
+*********************************************************************/
+
+int GenericApp_SendBroadcastMessage(char *fmt, ...)
+{
+  char buf[AIR_BUF_MAXLEN+2];
+  uint8 buflen;
+  
+  va_list arg_ptr;
+  va_start(arg_ptr, fmt);
+  vsprintf(buf, fmt, arg_ptr);
+  va_end(arg_ptr);
+  
+  sprintf(buf,"%s$",buf);
+  buflen = strlen(buf);
+
+  if ( AF_DataRequest( &GenericApp_Periodic_DstAddr, &GenericApp_epDesc,
+                       GENERICAPP_PERIODIC_CLUSTERID,
+                       buflen,
+                       (uint8*)buf,
+                       &GenericApp_TransID,
+                       AF_DISCV_ROUTE,
+                       AF_DEFAULT_RADIUS ) == afStatus_SUCCESS )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int GenericApp_SendMulticastMessage(char *fmt, ...)
+{
+  char buf[AIR_BUF_MAXLEN+2],buflen;
+  
+  va_list arg_ptr;
+  va_start(arg_ptr, fmt);
+  vsprintf(buf, fmt, arg_ptr);
+  va_end(arg_ptr);
+  
+  sprintf(buf,"%s$",buf);
+  buflen = strlen(buf);
+  
+  if ( AF_DataRequest( &GenericApp_Flash_DstAddr, &GenericApp_epDesc,
+                       GENERICAPP_FLASH_CLUSTERID,
+                       buflen,
+                       (uint8*)buf,
+                       &GenericApp_TransID,
+                       AF_DISCV_ROUTE,
+                       AF_DEFAULT_RADIUS ) == afStatus_SUCCESS )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int GenericApp_SendOnDemandMessage(uint16 trage, char *fmt, ...)
+{
+  char buf[AIR_BUF_MAXLEN+2],buflen;
+  
+  va_list arg_ptr;
+  va_start(arg_ptr, fmt);
+  vsprintf(buf, fmt, arg_ptr);
+  va_end(arg_ptr);
+  
+
+  sprintf(buf,"%s$",buf);
+  buflen = strlen(buf);
+  
+  GenericApp_P2P_DstAddr.addr.shortAddr = trage;
+
+  if ( AF_DataRequest( &GenericApp_P2P_DstAddr, &GenericApp_epDesc,
+                       GENERICAPP_P2P_CLUSTERID,
+                       buflen,
+                       (uint8*)buf,
+                       &GenericApp_TransID,
+                       AF_DISCV_ROUTE,
+                       AF_DEFAULT_RADIUS ) == afStatus_SUCCESS )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
